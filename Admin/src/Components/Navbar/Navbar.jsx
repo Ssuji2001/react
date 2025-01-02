63eb580634a0448157d674e41f7bab0c78b79e03
Ssuji <sujisakthi2005@gmail.com>
2025-01-02 15:52:47 +0530

    change

--- Admin/src/Components/Navbar/Navbar.jsx
+++ Admin/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
-import navProfile from '../../assets/nav-Profile.svg'
-
+import React from 'react';
+import './Navbar.css';
+import navlogo from '../../assets/nav-logo.svg';
+// Removed unused navProfile import to prevent build errors
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-<img src={navlogo} alt="" className='nav-logo' />
-
+    <div className="navbar">
+      <img src={navlogo} alt="Navigation Logo" className="nav-logo" />
+      {/* Add additional elements or links for your Navbar as needed */}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
