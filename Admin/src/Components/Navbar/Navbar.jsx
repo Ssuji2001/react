050d98830c256a01e26111f7fe273788fa2b5d02
Ssuji <sujisakthi2005@gmail.com>
2025-01-03 15:22:29 +0530

    images change

--- Admin/src/Components/Navbar/Navbar.jsx
+++ Admin/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Navbar.css';
 import navlogo from '../../assets/nav-logo.svg';
-// Removed unused navProfile import to prevent build errors
+
 
 const Navbar = () => {
   return (
