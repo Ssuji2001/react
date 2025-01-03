2612e934c1bebbfcdef732ee168a17e1ea5936f1
Ssuji <sujisakthi2005@gmail.com>
2025-01-03 13:52:19 +0530

    image_url

--- backend/index.js
+++ backend/index.js
@@ -64,7 +64,7 @@ app.use('/images',express.static('upload/images'))
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success:1,
-        image_url:`/images/${req.file.filename}`
+        image_url:`https://react-git-main-sujis-projects-d50eb3dd.vercel.app/:${port}/images/${req.file.filename}`
     })
 })
 
