3d87f8b96917db9765f6000d46c0872b9350bf0b
Ssuji <sujisakthi2005@gmail.com>
2025-01-04 11:19:34 +0530

    image change

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin:"*",
       methods:["POST","GET"],
       credentials:true
   }))
