ec873f798f7cc56c371582464c0eddb2723ed267
Ssuji <sujisakthi2005@gmail.com>
2025-01-16 19:59:44 +0530

    allprodcts

--- backend/index.js
+++ backend/index.js
@@ -110,7 +110,7 @@ app.get("/allproducts", async (req, res) => {
   let products = await Product.find({});
   products = products.map((product) => ({
     ...product.toObject(),
-    image: product.image.startsWith("http") ? product.image : ${BASE_URL}${product.image},
+    image: product.image.startsWith("http") ? product.image : $`{BASE_URL}${product.image}`,
   }));
   res.json(products);
 });
